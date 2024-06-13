2fa6fbe1b42b891ed7202ccf73e38cf17b657f47
felipnrv <77867598+felipnrv@users.noreply.github.com>
2024-06-12 19:17:07 -0500

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -18,14 +18,7 @@ app = Flask(__name__,template_folder='plantilla') #se inicializa la aplicacion
 app.secret_key = 'key'
 
 config = {
-        "apiKey": "AIzaSyCC1C7eIcb6Q0-WeWKuzSrZNwVoSyVf8Lw",
-        "authDomain": "dbfrutas-dd3ba.firebaseapp.com",
-        "projectId": "dbfrutas-dd3ba",
-        "databaseURL": "https://dbfrutas-dd3ba-default-rtdb.firebaseio.com/",
-        "storageBucket": "dbfrutas-dd3ba.appspot.com",
-        "messagingSenderId": "404825544625",
-        "appId": "1:404825544625:web:efaa68356d01d1153ccf90",
-        "measurementId": "G-EMQTJ61ZL6"
+        
     }
 
 firebase = pyrebase.initialize_app(config)
